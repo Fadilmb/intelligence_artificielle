446a558a01c13f4faf511a45328c5f444781844b
Fadilmb <fadil.boodoo@etu.upmc.fr>
2019-02-01 14:27:34 +0100

    Create Struc.c

---
+++ Struc.c
@@ -0,0 +1,15 @@
+   Preview
+struct List_header {
+   int nb;
+   struct bmu* bmu_list;
+   struct bmu* last;
+};
+
+stuct bmu {
+   int l;
+   int c;
+   double activation;
+ };
+ 
+ voir comment est fait une liste chainé
+ l'envoyer au prof pour voir ce qu"il en pense 
